7a948183a5588acdeb91bc48de809010c8fd90ff
James Dumas <jamesdumas418@gmail.com>
2025-04-11 16:55:28 -0400

    styling WIP

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useSettingsStore } from '@/stores/settings'
+import ContainerBox from '@/components/ContainerBox.vue';
+
+const settingsStore = useSettingsStore()
+
+</script>
+
+<template>
+  <div class="crt"></div>
+  <div class="root-container">
+    <ContainerBox>
+      <h1>Hello World!</h1>
+    </ContainerBox>
+    <br>
+    <ContainerBox>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </ContainerBox>
+    <br>
+    <ContainerBox>
+      <img src="@/assets/images/marty.jpg" alt="Marty" height="500">
+    </ContainerBox>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/ContainerBox.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container-box">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.container-box {
+    width: calc(100% - $box-padding * 2 - $box-border-width * 2);
+    height: calc(100% - $box-padding * 2 - $box-border-width * 2);
+    padding: $box-padding;
+    border: $box-border-width solid $text-color;
+    border-radius: $box-border-radius;
+}
+</style>
